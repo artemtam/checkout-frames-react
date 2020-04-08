59b3d180a201697307ca1771c5e7fd7a12cfaddc
Artem <artem.tamoian@onoffapp.com>
2020-04-08 13:20:51 +0300

    Removed Storybook, replaced `tsc` with `microbundle` for better build, integrated `release-it` to automate publishing, removed unused dependencies, fixed ESLint config.

--- .storybook/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  stories: ['../src/**/*.story.tsx'],
-  addons: [
-    '@storybook/preset-create-react-app',
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-  ],
-};
